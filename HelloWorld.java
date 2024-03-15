fbf5ed02a6bc058f51bc3abfa7353579b7b954b2
iOsamaGH <ec23902@qmul.ac.uk>
2024-03-15 09:34:22 +0000

    changggges

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
         	// Print "Hello World!" to the console
-        	System.out.println("Hello World!");
+        	System.out.println("NOT Hello World!");
+			System.out.println("lefbvl");
     		}
 	}
